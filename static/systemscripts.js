148cb0ffc90fe7d8b948cbf5cd5a0063fbb890f0
Artemii <50341618+zeightOFFICIAL@users.noreply.github.com>
2022-11-13 17:33:53 +0300

    0032

--- static/systemscripts.js
+++ static/systemscripts.js
@@ -18,6 +18,29 @@ function onLoadSolar() {
     objClicked("systemInfo");
 }
 
+let slideIndex = 1;
+showSlides(slideIndex, 'solarSlide');
+
+function plusSlides(n, name)
+{
+    showSlides(slideIndex += n, name);
+}
+function currentSlide(n, name)
+{
+    showSlides(n, name);
+}
+
+function showSlides(n, name) {
+    let i;
+    let slides = document.getElementsByClassName(name);
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
 function objClicked(revealName) {
     hideAllInfo();
     document.getElementById(revealName).style.textIndent = '0';
