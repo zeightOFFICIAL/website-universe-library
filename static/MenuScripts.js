11ca909f638157ffec1e529df14b8b107b27402f
Artemii <50341618+zeightOFFICIAL@users.noreply.github.com>
2022-11-17 22:15:13 +0300

    0044.9

---
+++ static/MenuScripts.js
@@ -0,0 +1,13 @@
+function continueToLabel() {
+    document.getElementById('first').style.left = '-200vw';
+    document.getElementById('second').style.left = '0vw';
+}
+
+function continueToInfo() {
+    document.getElementById('second').style.left = '-200vw';
+    document.getElementById('third').style.left = '0vw';
+}
+
+function continueToCards() {
+    document.getElementById('fourth').style.left = '-200vw';
+}
