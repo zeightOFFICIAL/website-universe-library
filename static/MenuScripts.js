a5a11efbe084fb2654456147cc9221a6eb9480b3
Artemii <50341618+zeightOFFICIAL@users.noreply.github.com>
2022-11-18 01:57:18 +0300

    0046

--- static/MenuScripts.js
+++ static/MenuScripts.js
@@ -1,32 +1,51 @@
 function continueToLabel() {
     document.getElementById('first').style.left = '-200vw';
-    document.getElementById('second').style.left = '0vw';
+    document.getElementById('second').style.left = '0';
     document.getElementById('third').style.left = '200vw';
+    document.getElementById('four').style.left = '400vw';
 }
 
 function continueToInfo() {
     document.getElementById('first').style.left = '-400vw';
     document.getElementById('second').style.left = '-200vw';
-    document.getElementById('third').style.left = '0vw';
+    document.getElementById('third').style.left = '0';
+    document.getElementById('four').style.left = '200vw';
 }
 
 function continueToInfo2() {
-    
-}
-
-function continueToCards() {
     document.getElementById('first').style.left = '-600vw';
     document.getElementById('second').style.left = '-400vw';
     document.getElementById('third').style.left = '-200vw';
-    document.getElementById('four1').style.left = '0vw';
-    document.getElementById('four2').style.left = '0vw';
-    document.getElementById('four3').style.left = '0vw';
-    document.getElementById('four4').style.left = '0vw';
+    document.getElementById('four').style.left = '0';
 }
 
-// function continueToLast() {
-//     document.getElementById('first').style.left = '-200vw';
-//     document.getElementById('second').style.left = '-400vw';
-//     document.getElementById('third').style.left = '-600vw';
-// }
+function continueToCards() {
+    document.getElementById('first').style.left = '-800vw';
+    document.getElementById('second').style.left = '-600vw';
+    document.getElementById('third').style.left = '-400vw';
+    document.getElementById('four').style.left = '-200vw';
+    document.getElementById('five').style.left = '0vw';
+    document.getElementById('five').style.zIndex = 2;
+    document.getElementById('fiveinner').style.left = '0vw';
+}
+
+function continueToLast() {
+    document.getElementById('first').style.left = '-800vw';
+    document.getElementById('second').style.left = '-600vw';
+    document.getElementById('third').style.left = '-400vw';
+    document.getElementById('four').style.left = '-200vw';
+    document.getElementById('five').style.left = '0vw';
+    document.getElementById('five').style.zIndex = 2;
+    document.getElementById('fiveinner').style.left = '0vw';
+}
+
+function continueToFirst() {
+    document.getElementById('first').style.left = '0';
+    document.getElementById('second').style.left = '200vw';
+    document.getElementById('third').style.left = '400vw';
+    document.getElementById('four').style.left = '600vw';
+    document.getElementById('five').style.left = '400vw';
+    document.getElementById('five').style.zIndex = 0;
+    document.getElementById('fiveinner').style.left = '400vw';
+}
 
