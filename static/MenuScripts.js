cab10a169f5f0dfd0521152df3864195c6b14244
Artemii <50341618+zeightOFFICIAL@users.noreply.github.com>
2022-11-18 00:19:58 +0300

    0045.5

--- static/MenuScripts.js
+++ static/MenuScripts.js
@@ -1,17 +1,32 @@
 function continueToLabel() {
     document.getElementById('first').style.left = '-200vw';
     document.getElementById('second').style.left = '0vw';
+    document.getElementById('third').style.left = '200vw';
 }
 
 function continueToInfo() {
+    document.getElementById('first').style.left = '-400vw';
     document.getElementById('second').style.left = '-200vw';
     document.getElementById('third').style.left = '0vw';
 }
 
+function continueToInfo2() {
+    
+}
+
 function continueToCards() {
+    document.getElementById('first').style.left = '-600vw';
+    document.getElementById('second').style.left = '-400vw';
     document.getElementById('third').style.left = '-200vw';
     document.getElementById('four1').style.left = '0vw';
     document.getElementById('four2').style.left = '0vw';
     document.getElementById('four3').style.left = '0vw';
     document.getElementById('four4').style.left = '0vw';
 }
+
+// function continueToLast() {
+//     document.getElementById('first').style.left = '-200vw';
+//     document.getElementById('second').style.left = '-400vw';
+//     document.getElementById('third').style.left = '-600vw';
+// }
+
