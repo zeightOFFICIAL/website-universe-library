0052da6c2a8635e031144292679084ba03c7baae
Artemii <50341618+zeightOFFICIAL@users.noreply.github.com>
2023-04-16 22:14:00 +0300

    0050.80 rev 1

---
+++ static/Types/PanelType.py
@@ -0,0 +1,143 @@
+from static.Types.StyleType import Style, EventHandlers
+
+'''
+{
+    BASE
+        SIMPLE_TEXT
+            TOP_HEADER_TEXT
+            HEADER_TEXT
+                COMBINED_HEADER
+            COMBINED_SIMPLE
+        SIMPLE_IMAGE
+        SIMPLE_VIDEO
+        SLIDER
+        IMAGE_SLIDER
+}
+'''
+
+
+class BasePanel:
+    def __init__(self, id):
+        self.id = id
+
+
+class SimpleTextPanel(BasePanel):
+    def __init__(self, id, text, box_color, slider=False):
+        super().__init__(id)
+        self.style_box = Style.Box.TextInfo(
+            box_color[0], box_color[1], box_color[2], slider)
+        self.style_text = Style.Text.Normal()
+        self.text = text
+
+    def __str__(self):
+        return f'<div {self.style_box} class="{self.id}"><p {self.style_text}> {self.text} </p></div>'
+
+
+class TopHeaderTextPanel(SimpleTextPanel):
+    def __init__(self, id, header, text, box_color, title_color, slider=False):
+        super().__init__(id, text, box_color, slider)
+        self.style_header = Style.Text.Header1(title_color[0], title_color[1])
+        self.header = header
+
+    def __str__(self):
+        return f'<div {self.style_box} class="{self.id}"><h2 {self.style_header}> {self.header} </h2><p {self.style_text}> {self.text} </p></div>'
+
+
+class HeaderTextPanel(SimpleTextPanel):
+    def __init__(self, id, header, text, box_color, title_color, slider=False):
+        super().__init__(id, text, box_color, slider)
+        self.style_header = Style.Text.Header2(title_color[0], title_color[1])
+        self.header = header
+
+    def __str__(self):
+        return f'<div {self.style_box} class="{self.id}"><h3 {self.style_header}> {self.header} </h3><p {self.style_text}> {self.text} </p></div>'
+
+
+class CombinedHeaderPanel(HeaderTextPanel):
+    def __init__(self, id, header, text, image, box_color, title_color, order=1, slider=False):
+        super().__init__(id, header, text, box_color, title_color, slider)
+        self.style_image = Style.Image.Original()
+        self.image = image
+        self.order = order
+
+    def __str__(self):
+        if self.order == 1:
+            return f'<div {self.style_box} class="{self.id}"><h3 {self.style_header}> {self.header} </h2><p {self.style_text}> {self.text} </p><img src="{self.image}" {self.style_image}></div>'
+        elif self.order == 2:
+            return f'<div {self.style_box} class="{self.id}"><h3 {self.style_header}> {self.header} </h2><img src="{self.image}" {self.style_image}><p {self.style_text}> {self.text} </p></div>'
+        else:
+            return f'<div {self.style_box} class="{self.id}"><img src="{self.image}" {self.style_image}><h3 {self.style_header}> {self.header} </h2><p {self.style_text}> {self.text} </p></div>'
+
+
+class SimpleImagePanel(BasePanel):
+    def __init__(self, id, image, box_color, slider=False):
+        super().__init__(id)
+        self.style_box = Style.Box.ImageInfo(
+            box_color[0], box_color[1], box_color[2], slider)
+        self.style_image = Style.Image.Default()
+        self.image = image
+
+    def __str__(self):
+        return f'<div {self.style_box} class="{self.id}"><img src="{self.image}" {self.style_image}></div>'
+
+
+class CombinedSimplePanel(SimpleTextPanel):
+    def __init__(self, id, text, image, box_color, order=1, slider=False):
+        super().__init__(id, text, box_color, slider)
+        self.style_image = Style.Image.Original()
+        self.image = image
+        self.order = order
+
+    def __str__(self):
+        if self.order == 1:
+            return f'<div {self.style_box} class="{self.id}"><p {self.style_text}> {self.text} </p><img src="{self.image}" {self.style_image}></div>'
+        else:
+            return f'<div {self.style_box} class="{self.id}"><img src="{self.image}" {self.style_image}><p {self.style_text}> {self.text} </p></div>'
+
+
+class SimpleVideoPanel(BasePanel):
+    def __init__(self, id, url, box_color):
+        super().__init__(id)
+        self.style_box = Style.Box.ImageInfo(
+            box_color[0], box_color[1], box_color[2])
+        self.style_video = Style.Video.Normal()
+        self.url = url
+
+    def __str__(self):
+        return f'<div {self.style_box} class="{self.id}"><iframe {self.style_video} src="{self.url}"></iframe></div>'
+
+
+class PanelSlider(BasePanel):
+    def __init__(self, id, all_panels, box_color):
+        super().__init__(id)
+        self.hidden = Style.Misc.OverflowHidden()
+        self.box_style = Style.Box.SliderInfo(
+            box_color[0], box_color[1], box_color[2])
+        self.button_left = Style.Button.ReturnSliderLeft(box_color[0])
+        self.button_right = Style.Button.ReturnSliderRight(box_color[1])
+        self.all_panels = all_panels
+
+    def __str__(self):
+        prefix = f'<div {self.box_style}>'
+        panels_str = ""
+        for panel in self.all_panels:
+            panels_str += panel.__str__()
+        embed_ID = f"'{self.id}'"
+        postfix = f'<button {self.button_left } {EventHandlers.SlidesButton(-1., embed_ID)}>&#10094;</button><button {self.button_right} {EventHandlers.SlidesButton(1.,  embed_ID)}>&#10095;</button></div>'
+        return f'{prefix}{panels_str}{postfix}'
+
+
+class ImagePanelSlider(PanelSlider):
+    def __init__(self, id, all_panels, box_color):
+        super().__init__(id, all_panels, box_color)
+        self.box_style = Style.Box.ImageInfo(
+            box_color[0], box_color[1], box_color[2])
+
+    def __str__(self):
+        prefix = f'<div {self.box_style}>'
+        panels_str = ""
+        for panel in self.all_panels:
+            panels_str += panel.__str__()
+        embed_ID = f"'{self.id}'"
+        postfix = f'<button {self.button_left } {EventHandlers.SlidesButton(-1., embed_ID)}>&#10094;</button><button {self.button_right} {EventHandlers.SlidesButton(1.,  embed_ID)}>&#10095;</button></div>'
+        return f'{prefix}{panels_str}{postfix}'
---
+++ static/Types/StyleType.py
@@ -0,0 +1,141 @@
+class Style:
+    class Box:
+        @staticmethod
+        def TextInfo(a_color, b_color, c_color, slider=False):
+            if slider == True:
+                return ""
+            border = f'border-width:3px;border-style:solid;margin:40px 15px;border-image:linear-gradient(to right,{a_color},{b_color}) 1;'
+            shadow = f'box-shadow:0 0 20px {c_color};'
+            return f'style="{border+shadow}"'
+
+        @staticmethod
+        def SliderInfo(a_color, b_color, c_color):
+            border = f'border-width:3px;border-style:solid;margin:40px 15px;border-image:linear-gradient(to right,{a_color},{b_color}) 1;'
+            shadow = f'box-shadow:0 0 20px {c_color};'
+            overflow = f'overflow:hidden;'
+            return f'style="{border+shadow+overflow}"'
+
+        @staticmethod
+        def ImageInfo(a_color, b_color, c_color, slider=False):
+            if slider == True:
+                return ""
+            border = f'border-width:3px;border-style:solid;margin:40px 15px;border-image:linear-gradient(to right,{a_color},{b_color}) 1;'
+            shadow = f'box-shadow:0 0 20px {c_color};'
+            filling = f'background-color:{a_color};background-image:linear-gradient(to right, {a_color}, {b_color});'
+            return f'style="{border+shadow+filling}"'
+
+    class Text:
+        @staticmethod
+        def Normal():
+            style = f'text-align:center;font-family:\'normal\';font-size:3vmin;cursor:default;color:#fff;padding-left:20px;padding-right:20px;'
+            return f'style="{style}"'
+
+        @staticmethod
+        def Header1(a_color, b_color):
+            style = f'text-align:center;font-family:\'solar\';font-size:7vmin;margin-bottom:2.5vmin;cursor:default;padding-left:20px;padding-right:20px;'
+            font = f'background-color:{a_color};background-image:linear-gradient(62deg, {a_color} 0%, {b_color} 100%);background-clip:text;-webkit-background-clip:text;-webkit-text-fill-color:transparent;'
+            return f'style="{style+font}"'
+
+        @staticmethod
+        def Header2(a_color, b_color):
+            style = f'text-align:center;font-family:\'solar\';font-size:5.5vmin;margin-bottom:2.5vmin;cursor:default;padding-left:20px;padding-right:20px;'
+            font = f'background-color:{a_color};background-image:linear-gradient(62deg, {a_color} 0%, {b_color} 100%);background-clip:text;-webkit-background-clip:text;-webkit-text-fill-color:transparent;'
+            return f'style="{style+font}"'
+
+    class Image:
+        @staticmethod
+        def Default():
+            style = f'width:100%;height:45vmin;object-fit:cover;'
+            return f'style="{style}"'
+
+        @staticmethod
+        def Original():
+            style = f'width:100%;object-fit:cover;'
+            return f'style="{style}"'
+
+    class Video:
+        @staticmethod
+        def Normal():
+            arg = f'id="video" width="100%" height="315"'
+            specifics = f'title="" frameborder="0" allow="accelerometer;autoplay;clipboard-write;encrypted-media;gyroscope;picture-in-picture"'
+            return f'{arg+specifics} allowfullscreen'
+
+    class Misc:
+        @staticmethod
+        def OverflowHidden():
+            return f'style="overflow:hidden;"'
+
+        @staticmethod
+        def AccentBar(a_color, b_color):
+            sizes = f'top:0;left:0;width:10vmin;height:100vh;'
+            animations = f'transition:left .7s;transition:right .7s;'
+            position = f'position:absolute;'
+            color = f'background-color:{a_color};background-image:linear-gradient(62deg, {a_color} 0%, {b_color} 100%);box-shadow:0 0 3vw {a_color};'
+            return f'style="{sizes+animations+position+color}"'
+
+    class Button:
+        @staticmethod
+        def ReturnSliderLeft(a_color):
+            interactive = f'display:inline;color:#000;cursor:pointer;transition:.5s;background:transparent;border:none;'
+            text = f'font-size:2vmin;background-color:{a_color};margin:0;padding:0;overflow:hidden;'
+            position = f'float:left;width:50%;'
+            return f'style="{interactive+text+position}"'
+
+        @staticmethod
+        def ReturnSliderRight(a_color):
+            interactive = f'display:inline;color:#000;cursor:pointer;transition:.5s;background:transparent;border:none;'
+            text = f'font-size:2vmin;background-color:{a_color};margin:0;padding:0;overflow:hidden;'
+            position = f'float:right;width:50%;'
+            return f'style="{interactive+text+position}"'
+
+
+class Visual:
+    class Object:
+        def __init__(self, args, z_index):
+            self.size = args[0]
+            self.margins = args[1]
+            self.a_color = args[2]
+            self.b_color = args[3]
+            self.lumin = args[4]
+            self.z_index = z_index
+            self.c_color = self.b_color if len(args) < 6 else args[5]
+
+        def __str__(self):
+            size = f'height:{self.size};width:{self.size};'
+            margins = f'margin-top:{self.margins};margin-left:{self.margins};'
+            fill = f'background:{self.a_color};background:radial-gradient(circle, {self.a_color} 0%, {self.b_color} 100%);cursor:pointer;'
+            lumin = f'box-shadow 0 0 {self.lumin} {self.c_color};'
+            zindex = f'z-index:{self.z_index};' if self.z_index != -998 else ""
+            return f'style="{size+margins+fill+lumin+zindex}"'
+
+    class Orbit:
+        def __init__(self, args):
+            self.size = args[0]
+            self.margins = args[1]
+
+        def __str__(self):
+            size = f'width:{self.size};height:{self.size};margin-top:{self.margins};margin-left:{self.margins};'
+            return f'class="orbit" style="{size}"'
+
+    class Spin:
+        def __init__(self, args, z_index):
+            self.size = args[0]
+            self.margins = args[1]
+            self.period = args[2]
+            self.z_index = z_index
+
+        def __str__(self):
+            size = f'width:{self.size};height:{self.size};'
+            margins = f'margin-top:{self.margins};margin-left:{self.margins};'
+            animation = f'animation:spin-right {self.period} linear infinite;border-radius:100%;'
+            return f'style="{size+margins+animation};z-index:{self.z_index};"'
+
+
+class EventHandlers:
+    class SlidesButton:
+        def __init__(self, args, slider_name):
+            self.args = args
+            self.name = slider_name
+
+        def __str__(self):
+            return f'onclick="plusSlides({self.args},{self.name});"'
