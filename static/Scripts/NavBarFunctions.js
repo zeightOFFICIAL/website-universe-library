b190d932ade90a3fd3f9fd9c91e5239410453716
Artemii <50341618+zeightOFFICIAL@users.noreply.github.com>
2023-11-21 16:47:37 +0300

    new

--- static/Scripts/NavBarFunctions.js
+++ static/Scripts/NavBarFunctions.js
@@ -258,6 +258,9 @@ function navPanelEnter() {
     var btn3 = document.getElementById("CALC_BUTTON");
     var btn4 = document.getElementById("OBJS_BUTTON");
     var btn5 = document.getElementById("QUIZ_BUTTON");
+    var other = document.getElementById("LOWER_PART");
+
+    other.style.filter = "brightness(50%)";
 
     nav.style.height = "25%";
     nav.style.filter = "brightness(40%)";
@@ -290,6 +293,9 @@ function navPanelLeave() {
     var btn3 = document.getElementById("CALC_BUTTON");
     var btn4 = document.getElementById("OBJS_BUTTON");
     var btn5 = document.getElementById("QUIZ_BUTTON");
+    var other = document.getElementById("LOWER_PART");
+
+    other.style.filter = "brightness(100%)";
 
     nav.style.height = "8%";
     nav.style.filter = "brightness(100%)";
