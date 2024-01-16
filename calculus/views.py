5c60d67be36a37856a6071bc994926c2bdc0be9f
Artemii <artemsaganenko@gmail.com>
2024-01-16 10:09:10 +0300

    updated to calc

--- calculus/views.py
+++ calculus/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def calculus_view(request):
-    return render(request, "Calculus.html")
+    Head = '<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"><link rel="icon" href="../static/Icons/LogoStrip.ico" charset="UTF-8"><title>Universe Library</title>'
+    return render(request, "Calculus.html", {"head": Head})
